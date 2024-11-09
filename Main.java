32e6e78f5b6752a6dce5d88a3f98412bb3161435
tom3384 <t_nguyen117@u.pacific.edu>
2024-11-09 04:38:20 +0000

    Fixed count8 bug

--- Main.java
+++ Main.java
@@ -10,16 +10,16 @@ public class Main {
 
     public static int count8(int number) {
         // Base case for recursion
-        if (n ==0) {
+        if (number == 0) {
             return 0;
         }
 
         // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6) 
-        int lastDigit = n % 10; 
+        int lastDigit = number % 10; 
 
         // divide (/) by 10 removes the rightmost digit (126 / 10 is 12). 
         // So first we remove the right most digit, then we take the new most rightmost digit (which is the second last digit)
-        int secondLastDigit = (n / 10) % 10; 
+        int secondLastDigit = (number / 10) % 10; 
 
         // We are counting the occurences of 8's
         if (lastDigit == 8) {
@@ -27,14 +27,14 @@ public class Main {
             if (secondLastDigit == 8) {
                 // Counts it as double because it has an 8 to the immediate left
                 // We return count8(n/10) because we want to move on to the next digit using recursion
-                return 2 + count8(n / 10);                
+                return 2 + count8(number / 10);                
             } else {
                 // Found 1 occurence of 8
-                return 1 + count8(n / 10); 
+                return 1 + count8(number / 10); 
             }
         } else {
             // If the last digit isn't 8.
-            return count8(n / 10);
+            return count8(number / 10);
         }
     }
 }
